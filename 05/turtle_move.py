6df4e274bff71a9db04280c483327a7e560cd245
dbwodn1320 <yjw1320@gmail.com>
2021-09-13 15:15:23 +0900

    drill 05 완성 수정본
    2017180024 유재우
    https://youtu.be/X6ijcOjdMlo

--- 05/turtle_move.py
+++ 05/turtle_move.py
@@ -2,21 +2,21 @@ import turtle
 
 
 def turtle_move_up():
+    turtle.stamp()
     turtle.setheading(90)
     turtle.forward(50)
-    turtle.stamp()
 def turtle_move_down():
+    turtle.stamp()
     turtle.setheading(270)
     turtle.forward(50)
-    turtle.stamp()
 def turtle_move_left():
+    turtle.stamp()
     turtle.setheading(180)
     turtle.forward(50)
-    turtle.stamp()
 def turtle_move_right():
+    turtle.stamp()
     turtle.setheading(0)
     turtle.forward(50)
-    turtle.stamp()
 def turtle_move_reset():
     turtle.reset()
 
